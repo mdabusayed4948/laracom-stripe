b7f848a824cc09c9af3976a9abf3994d69ff4069
sayedme120 <sayedme120@gmail.com>
2020-08-04 04:48:14 +0600

    initial setup project and crud functionality add

---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Create Product</div>
+
+                    <div class="card-body">
+                        <form action="{{ route('products.store') }}" method="post" enctype="multipart/form-data">
+                            {{ csrf_field() }}
+                            <div class="form-group">
+                                <label for="name">Name</label>
+                                <input type="text" name="name" class="form-control" value="{{ old('name') }}">
+                            </div>
+                            <div class="form-group">
+                                <label for="image">Price</label>
+                                <input type="number" name="price" class="form-control" value="{{ old('price') }}">
+                            </div>
+                            <div class="form-group">
+                                <label for="image">Image</label>
+                                <input type="file" name="image" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="description">Description</label>
+                                <textarea name="description" id="description" cols="30" rows="10" class="form-control">{{ old('description') }}</textarea>
+                            </div>
+                            <div class="form-group">
+                                <button class="form-control btn btn-success">Save Product</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+
---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Products</div>
+
+                    <div class="card-body">
+                        <table class="table">
+                            <thead>
+                            <th>
+                                Name
+                            </th>
+                            <th>
+                                Price
+                            </th>
+                            <th>
+                                Edit
+                            </th>
+                            <th>
+                                Delete
+                            </th>
+                            </thead>
+                            <tbody>
+                            @foreach($products as $product)
+                                <tr>
+                                    <td>{{ $product->name }}</td>
+                                    <td>{{ $product->price }}</td>
+                                    <td>
+                                        <a href="{{ route('products.edit', ['id' => $product->id ]) }}" class="btn btn-primary btn-xs">Edit</a>
+                                    </td>
+                                    <td>
+                                        <form action="{{ route('products.destroy', ['id' => $product->id ]) }}" method="post">
+                                            {{ csrf_field() }}
+                                            {{ method_field('DELETE') }}
+                                            <button class="btn btn-xs btn-danger">Delete</button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+
